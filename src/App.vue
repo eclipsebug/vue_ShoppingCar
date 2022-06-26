5daceb7f8d96d85afd860e40259aba253397f783
eclipsebug <860337520@qq.com>
2022-06-26 17:29:49 +0800

    完成了页面显示

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <div></div>
+  <div>
+    <MyHeader />
+    <MyGoods />
+    <MyFooter />
+  </div>
 </template>
 
 <script>
-export default {}
+// 引入
+import axios from 'axios'
+import MyHeader from '#/MyHeader.vue'
+import MyGoods from '#/MyGoods.vue'
+import MyFooter from '#/MyFooter.vue'
+export default {
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter
+  },
+  created() {
+    this.getGoods()
+  },
+  methods: {
+    async getGoods() {
+      const res = await axios({
+        url: '/api/cart'
+      })
+      console.log(res)
+    }
+  }
+}
 </script>
 
 <style></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 3000,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        '#': path.join(__dirname, 'src/components')
+      }
+    }
+  }
 })
